c422fc24e4359ba97118daf6a20f2007de34443a
Ehsan Azarnasab <dashesy@gmail.com>
2015-08-28 08:32:30 -0700

    add missing compat header

---
+++ cbmex/mex_compat.h
@@ -0,0 +1,14 @@
+#ifndef _MEX_COMPAT_H
+#define _MEX_COMPAT_H
+
+// compatibility layer for Octave
+
+#ifndef MAX_int16_T
+#define MAX_int16_T                    ((int16_T)(32767))
+#define MIN_int16_T                    ((int16_T)(-32768))
+#define MAX_uint16_T                   ((uint16_T)(65535U))
+#define MIN_uint16_T                   ((uint16_T)(0U))
+#endif
+
+#endif
+
