a19ce7813d98398061b7bbb10993a0e241240126
Chadwick Boulay <chadwick.boulay@gmail.com>
2025-10-17 19:42:00 -0400

    Python version from git tag

--- bindings/Python/cerebus/__init__.py
+++ bindings/Python/cerebus/__init__.py
@@ -1,1 +1,5 @@
-__version__ = 0.5
+try:
+    from cerebus.__version__ import version as __version__
+except ImportError:
+    # Version file not generated yet (e.g., in development before first build)
+    __version__ = "0.0.0.dev0"
